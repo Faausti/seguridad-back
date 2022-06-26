6af35b3234a5d13d5b1518c59dcb2e2ba3ea07ed
flestrade <flestrade@spacedev.uy>
2022-06-25 23:08:03 -0300

    feat: log in ended

--- server.js
+++ server.js
@@ -37,23 +37,48 @@ poolPepper.query("SELECT * from userpepper", (err, res) => {
   poolPepper.end()
 });
 
-app.post("/login", (req, res) => {
-  // Insert Login Code Here
-  let userMail = req.body.userMail;
-  let password = req.body.password;
-  const salt = bcrypt.genSaltSync();
-  let isValidMail = /[^'?="!]/g.test(userMail)
-  let isValidPass = /[^'?="!]/g.test(password)
-  if (isValidPass && isValidMail){
-    const passwordHashed = bcrypt.hashSync(pepper + password, salt);
-    poolUser.query(
-      `INSERT INTO userInfo(id, mail, password, salt) VALUES ('${crypto.randomUUID()}', '${userMail}', '${passwordHashed}', '${salt}')`,
-      (err, res) => {
-        console.log(err, res);
-        poolUser.end();
-      }
-    );
-  }
+// app.post("/login", (req, res) => {
+//   // Insert Login Code Here
+//   let userMail = req.body.userMail;
+//   let password = req.body.password;
+//   const salt = bcrypt.genSaltSync();
+//   let isValidMail = /[^'?="!]/g.test(userMail)
+//   let isValidPass = /[^'?="!]/g.test(password)
+//   if (isValidPass && isValidMail){
+//     const passwordHashed = bcrypt.hashSync(pepper + password, salt);
+//     poolUser.query(
+//       `INSERT INTO userInfo(id, mail, password, salt) VALUES ('${crypto.randomUUID()}', '${userMail}', '${passwordHashed}', '${salt}')`,
+//       (err, res) => {
+//         console.log(err, res);
+//         poolUser.end();
+//       }
+//     );
+//   }
 
-  res.send(`Username: ${userMail} Password: ${password}`);
+  app.post("/login", (req, res) => {
+    // Insert Login Code Here
+    let userMail = req.body.userMail;
+    let password = req.body.password;
+    let isValidMail = /[^'?="!]/g.test(userMail)
+    let isValidPass = /[^'?="!]/g.test(password)
+    if (isValidPass && isValidMail) {
+      poolUser.query(`SELECT * from userinfo WHERE mail= '${userMail}'`, (err, resp) => {
+        if (resp?.rows[0]){
+          const passwordHashed = bcrypt.hashSync(pepper + password, resp.rows[0].salt);
+          const passwordDB = resp.rows[0].password
+          if (passwordDB === passwordHashed){
+             res.status(200).send('Log in correcto')
+          }else{
+            res.status(400).send('Contraseña incorrecta')
+          }
+        }else{
+          res.status(404).send('No hay usuario con este mail')
+        }
+
+        poolUser.end();
+      });
+      
+    }else{
+      res.status(400).send( 'Mail o contraseña no valido')
+    }
 });
